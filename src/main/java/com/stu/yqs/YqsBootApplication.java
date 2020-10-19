8ea6e27805ca07062c882024412243f92893cf6e
gzs15406 <yangfan14>
2020-10-19 19:56:17 +0800

    format

--- src/main/java/com/stu/yqs/YqsBootApplication.java
+++ src/main/java/com/stu/yqs/YqsBootApplication.java
@@ -1,7 +1,6 @@
 package com.stu.yqs;
 
 import org.mybatis.spring.annotation.MapperScan;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,16 +8,17 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.PropertySource;
 
 
-@SpringBootApplication(scanBasePackages={"com.stu.yqs.*"})
+@SpringBootApplication(scanBasePackages = {"com.stu.yqs.*"})
 @MapperScan(basePackages = "com.stu.yqs.dao")
-@PropertySource(value ="classpath:yqs.properties", encoding = "utf-8")
-public class YqsBootApplication extends SpringBootServletInitializer{
+@PropertySource(value = "classpath:yqs.properties", encoding = "utf-8")
+public class YqsBootApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YqsBootApplication.class, args);
-	}
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-	  return builder.sources(this.getClass());
-	 }
+    public static void main(String[] args) {
+        SpringApplication.run(YqsBootApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
